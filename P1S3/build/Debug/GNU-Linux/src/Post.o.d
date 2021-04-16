82f0fbbcd8e1b2322770bd89fb653a15606fd342
oscarntnz <oscarntnz@gmail.com>
2021-04-16 14:41:38 +0200

    P2

--- P1S3/build/Debug/GNU-Linux/src/Post.o.d
+++
@@ -1,5 +0,0 @@
-build/Debug/GNU-Linux/src/Post.o: src/Post.cpp src/Post.h src/Usuario.h
-
-src/Post.h:
-
-src/Usuario.h:
--- P1S3/build/Debug/GNU-Linux/src/Usuario.o.d
+++
@@ -1,3 +0,0 @@
-build/Debug/GNU-Linux/src/Usuario.o: src/Usuario.cpp src/Usuario.h
-
-src/Usuario.h:
