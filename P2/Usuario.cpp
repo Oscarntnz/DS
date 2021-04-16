7acbaca2462c0bc41f7e26c533984438bad2d815
oscarntnz <oscarntnz@gmail.com>
2021-04-16 17:27:25 +0200

    P2

--- P2/Usuario.cpp
+++
@@ -1,6 +0,0 @@
-#include "Usuario.h"
-
-Usuario::Usuario(const std::string & nombreUsuario, const std::string & password){
-    this->nombreUsuario = nombreUsuario;
-    this->password = password;
-}
