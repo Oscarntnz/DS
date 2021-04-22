296b5d4f8ff11f1828bce4544b6179a6b4c6ec5d
oscarntnz <oscarntnz@gmail.com>
2021-04-22 11:20:28 +0200

    P2

---
+++ P2/Constructor.dart
@@ -0,0 +1,39 @@
+import 'Gestor.dart';
+//import 'FiltroEtiquetas.dart';
+//import 'FiltroCensura.dart';
+import 'Post.dart';
+import 'Usuario.dart';
+
+class Constructor {
+  Constructor._constructorPrivado();
+
+  static final Constructor _instance = Constructor._constructorPrivado();
+
+  factory Constructor() {
+    return _instance;
+  }
+
+  Gestor build() {
+    Gestor gestor = new Gestor();
+    /*AdminFiltros adminFiltros;
+    FiltroEtiquetas fEtiq;
+    FiltroCensura fCen;
+
+    adminFiltros.setFiltro(fEtiq);
+    adminFiltros.setFiltro(fCen);
+
+    gestor.setAdminFiltros(adminFiltros);*/
+
+    Usuario u1 = gestor.registrar("mistyroasted", "1234");
+    Usuario u2 = gestor.registrar("viewbeach", "4321");
+    Usuario u3 = gestor.registrar("cartierfalter", "13245");
+
+    gestor.publicarPost("esta es la palla que he hecho hoy #paella", u1);
+    gestor.publicarPost(
+        "hoy he hecho unas croquetas para comer #croquetas #delicia", u2);
+    gestor.publicarPost(
+        "hello world yo tambien quiero croquetas #croquetas", u3);
+
+    return gestor;
+  }
+}
