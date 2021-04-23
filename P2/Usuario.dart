32302a8e9a693ff44e7b4203a58c90e1892250d7
amxndam <amanmoy@gmail.com>
2021-04-23 03:48:14 +0200

    seguir usuarios

--- P2/Usuario.dart
+++ P2/Usuario.dart
@@ -1,17 +1,36 @@
 class Usuario {
-    String nombreUsuario;
-    String password;
+  String nombreUsuario;
+  String password;
+  List<Usuario> seguidos;
 
-    String getNombre(){
-      return nombreUsuario;
-    }
+  String getNombre(){
+    return nombreUsuario;
+  }
 
-    String getPassword(){
-      return password;
-    }
+  bool isSeguido(Usuario usu){
+    if(seguidos.contains(usu))
+      return true;
 
-    Usuario(String nombreUsuario, String password){
-      this.nombreUsuario = nombreUsuario;
-      this.password = password;
-    }
+    else
+      return false;
+  }
+
+  bool isSeguidor(Usuario usu){
+    if(usu.isSeguido(this))
+      return true;
+
+    else
+      return false;
+  }
+
+  void seguir(Usuario usu){
+    if(!isSeguido(usu))
+      seguidos.add(usu);
+
+  }
+
+  Usuario(String nombreUsuario, String password){
+    this.nombreUsuario = nombreUsuario;
+    this.password = password;
+  }
 }
