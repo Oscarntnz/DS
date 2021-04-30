828e7f39e085f11224d38edaba0893cfdd192109
amxndam <amanmoy@gmail.com>
2021-04-30 04:56:45 +0200

    test añadidos a widget_test.dart

--- P3/test/widget_test.dart
+++ P3/test/widget_test.dart
@@ -4,27 +4,53 @@
 // utility that Flutter provides. For example, you can send tap and scroll
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:p_2/main.dart';
+import 'package:p_2/controlador/Gestor.dart';
+import 'package:p_2/vista/Home.dart';
+import 'package:p_2/vista/Perfil.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+  Gestor().inicializar();
+  Gestor().login('mistyroasted', '1234');
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+  Widget home = new MediaQuery(
+      data: new MediaQueryData(),
+      child: new MaterialApp(home: new Home())
+  );
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+
+  testWidgets('Visualización de posts', (WidgetTester tester) async {
+
+    await tester.pumpWidget(home);
+
+    var listPosts = find.byType(ListView);
+    expect(listPosts, findsOneWidget);
+
   });
+
+  testWidgets('botón publicar post', (WidgetTester tester) async {
+
+    await tester.pumpWidget(home);
+
+    await tester.tap(find.byType(FloatingActionButton));
+    await tester.pumpAndSettle();
+
+    expect(find.widgetWithIcon(FloatingActionButton, Icons.add), findsOneWidget);
+  });
+
+  testWidgets('Busqueda correcta', (WidgetTester tester) async {
+
+    await tester.pumpWidget(home);
+
+    await tester.tap(find.byIcon(Icons.search));
+    await tester.pumpAndSettle();
+    await tester.enterText(find.byType(TextField), 'a');
+    await tester.testTextInput.receiveAction(TextInputAction.done);
+    await tester.pumpAndSettle();
+
+    expect(find.text("esta es la palla que he hecho hoy #paella"), findsOneWidget);
+  });
+
 }
